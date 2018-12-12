1fe5708a63c3617c3d87c2e98578b2245471b295
Milonguero <45806175+Milonguero@users.noreply.github.com>
2018-12-12 00:28:17 -0500

    Update temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -8,9 +8,17 @@ if choice == 1:
     celsius=int(input("Enter the current temperature in celsius? ")) 
     far= celsius * 9/5 +32
     print ("The temperature is %d degrees in Farenheit!"%(far))
+    if far > 95:
+        print("Wow! It's hot! Turn on the AC")
+    if far < 20:
+        print("It's cold out there, better bundle up!")
 elif choice == 2:
     farenheit=int(input("Enter the current temperature in Farenheit? ")) 
     cel= (farenheit-32) * 5/9
     print ("The temperature is %d degrees in Celsius!"%(cel))
+    if cel > 33:
+        print("Wow! It's hot! Turn on the AC")
+    if cel < -6:
+        print("It's cold out there, better bundle up!")
 elif choice == 3:
      print("goodbye")
